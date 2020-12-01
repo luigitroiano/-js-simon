10dcd90cf0f1fd76335637e23d040d1b4b24b265
LuigiTroiano <troiano.luigi8@gmail.com>
2020-12-01 18:08:35 +0100

    Esercizio Completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,31 +16,34 @@ $(function() {
   alert('Numeri random: ' + arrayNumber + '\nProva a memorizzare questi numeri...sarai in grado di ricordarli?\nPremi "ok" per scoprirlo.');
 
   // Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
-  var seconds = 5;
+  var seconds = 30;
   var interval = setInterval(function() {
     $('#secondi').html(seconds);
     if (seconds === 0) {
       // $('#secondi').hide();
       for (var index = 0; index < arrayNumber.length; index++) {
-        var numberGuessed = Number(prompt('Inserisci uno dei numeri precedenti'));
-        arrayGuessed.push(numberGuessed);
+        var numberUser = Number(prompt('Inserisci uno dei numeri precedenti'));
+        arrayUser.push(numberUser);
+        if (arrayNumber.indexOf(numberUser) !== -1) {
+          arrayGuessed.push(numberUser);
+        } 
       };
-      console.log(arrayGuessed);
+      console.log(arrayUser);
+      // $('#secondi').hide();
       clearInterval(interval);
     } else {
       seconds--;
-    }
-    
+    }  
   }, 1000);
   
+  var arrayUser = [];
   var arrayGuessed = [];
-  
-  
+  // Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+  setTimeout(myFunction, 31000);
 
-
-
-
-
+  function myFunction() {
+    alert('I numeri scelti dal pc randomicamente sono: ' + arrayNumber + '\nI numeri che tu hai ricordato correttamente sono: ' + arrayGuessed);
+    alert('Punteggio: ' + arrayGuessed.length + '/5' + '\nPercentuale di successo: ' + (arrayGuessed.length/5*100).toFixed(0) + '%');
+  }
 });
 
-// Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
